2a9ba2e6f3f74cb0af64c50d5021fc1a05da1150
chrislee1996 <prepchrislee@gmail.com>
2022-03-14 15:30:26 -0400

    updated profilejs to reflect a users profile and removed unneeded liquid files - also updated so when logged in, user can now do stuff

--- controllers/profile.js
+++ controllers/profile.js
@@ -21,10 +21,10 @@ router.use((req, res, next) => {
 
 // Routes
 
-// index ALL
+// index page
 router.get('/', (req, res) => {
 	Profile.find({})
-		.then(examples => {
+		.then(profiles => {
 			const username = req.session.username
 			const loggedIn = req.session.loggedIn
 			
@@ -35,91 +35,7 @@ router.get('/', (req, res) => {
 		})
 })
 
-// index that shows only the user's examples
-router.get('/mine', (req, res) => {
-    // destructure user info from req.session
-    const { username, userId, loggedIn } = req.session
-	Profile.find({ owner: userId })
-		.then(profiles => {
-			res.render('profiles/index', { profiles, username, loggedIn })
-		})
-		.catch(error => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
 
-// new route -> GET route that renders our page with the form
-router.get('/new', (req, res) => {
-	const { username, userId, loggedIn } = req.session
-	res.render('profiles/new', { username, loggedIn })
-})
-
-// create -> POST route that actually calls the db and makes a new document
-router.post('/', (req, res) => {
-	req.body.ready = req.body.ready === 'on' ? true : false
-
-	req.body.owner = req.session.userId
-	Profile.create(req.body)
-		.then(profile => {
-			console.log('this was returned from create', example)
-			res.redirect('/profiles')
-		})
-		.catch(error => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
-// edit route -> GET that takes us to the edit form view
-router.get('/:id/edit', (req, res) => {
-	// we need to get the id
-	const profileId = req.params.id
-	Profile.findById(exampleId)
-		.then(profile => {
-			res.render('profiles/edit', { profile })
-		})
-		.catch((error) => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
-// update route
-router.put('/:id', (req, res) => {
-	const profileId = req.params.id
-	req.body.ready = req.body.ready === 'on' ? true : false
-
-	Profile.findByIdAndUpdate(profileId, req.body, { new: true })
-		.then(profile => {
-			res.redirect(`/profiles/${profile.id}`)
-		})
-		.catch((error) => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
-// show route
-router.get('/:id', (req, res) => {
-	const profileId = req.params.id
-	Profile.findById(exampleId)
-		.then(profile => {
-            const {username, loggedIn, userId} = req.session
-			res.render('profiles/show', { profile, username, loggedIn, userId })
-		})
-		.catch((error) => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
-// delete route
-router.delete('/:id', (req, res) => {
-	const profilesId = req.params.id
-	Profile.findByIdAndRemove(exampleId)
-		.then(profile => {
-			res.redirect('/profiles')
-		})
-		.catch(error => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
 
 // Export the Router
 module.exports = router
