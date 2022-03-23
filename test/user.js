899a1d09ff37b56858b4ab2c4de45f6443658561
chrislee1996 <prepchrislee@gmail.com>
2022-03-22 21:38:33 -0400

    updated test file

--- test/user.js
+++ test/user.js
@@ -2,7 +2,11 @@ const bcrypstjs = require('bcryptjs')
 const User = require('../models/user')
 
 const chai = require('chai')
+const chaiHttp = require('chai-http')
+const server = require('../server')
 const assert = chai.assert
+
+chai.use(chaiHttp)
 
 
 
